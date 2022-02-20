65da3ac793ed23c881203229e3c7d8d813af94c8
nazhimkalam <nazhimkalam@gmail.com>
2022-02-20 18:58:11 +0530

    using local images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Image from 'next/image'
+import localImage from '../public/mountains.jpg'
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div style={{ margin: '2pc 5pc' }}>
+      <h1>Image Optimization using NextJs</h1>
+      <section>
+        <h2>1. Using local images</h2>
+        <Image
+          src={localImage}
+          alt="Picture of the author"
+          // width={500} automatically provided
+          // height={500} automatically provided
+          // blurDataURL="data:..." automatically provided
+          // placeholder="blur" // Optional blur-up while loading
+        />
+      </section>
+
+      <section></section>
+
+    </div>
   )
 }
 
