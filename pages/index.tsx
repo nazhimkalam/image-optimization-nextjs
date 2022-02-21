b34df26609288b5c3bcf93a7d80671683d378a59
nazhimkalam <nazhimkalam@gmail.com>
2022-02-21 21:40:57 +0530

    used plaiceholder for blurring effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
 import localImage from "../public/mountains.jpg";
 
-const HomePage = () => {
-  const remoteImage = "https://images.unsplash.com/photo-1525824236856-8c0a31dfe3be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHdhdGVyZmFsbHxlbnwwfHwwfHw%3D";
-
+const HomePage = ({ imageProps }: any) => {
   return (
     <div style={{ margin: "2pc 5pc" }}>
       <h1>Image Optimization using NextJs</h1>
@@ -14,7 +13,7 @@ const HomePage = () => {
           alt="Picture of the author"
           // width={500} automatically provided
           // height={500} automatically provided
-          // blurDataURL={`data:image/jpeg:base64,${remoteImage}`} automatically provided
+          // blurDataURL={imageProps.blurDataURL} automatically provided
           // placeholder="blur" // Optional blur-up while loading
         />
       </section>
@@ -22,7 +21,7 @@ const HomePage = () => {
       <section>
         <h2>2. Remote Images (without onload blur)</h2>
         <Image
-          src={remoteImage}
+          src={imageProps.remoteImage}
           alt="Picture of the author"
           width={400}
           height={500}
@@ -31,43 +30,42 @@ const HomePage = () => {
 
       <section>
         <h2>3. Remote Images (with onload bluring)</h2>
-        <div style={{ display: "flex;" }}>
+        <div style={{ display: "flex" }}>
           <Image
-            src={remoteImage}
+            src={imageProps.remoteImage}
             alt="Picture of the author"
-            blurDataURL={`data:image/jpeg:base64,${remoteImage}`}
+            blurDataURL={imageProps.blurDataURL}
             placeholder="blur"
             width={200}
             height={250}
           />
           <Image
-            src={remoteImage}
+            src={imageProps.remoteImage}
             alt="Picture of the author"
-            blurDataURL={`data:image/jpeg:base64,${remoteImage}`}
+            blurDataURL={imageProps.blurDataURL}
             placeholder="blur"
             width={200}
             height={250}
           />
           <Image
-            src={remoteImage}
+            src={imageProps.remoteImage}
             alt="Picture of the author"
-            blurDataURL={`data:image/jpeg:base64,${remoteImage}`}
+            blurDataURL={imageProps.blurDataURL}
             placeholder="blur"
             width={200}
             height={250}
           />
           <Image
-            src={remoteImage}
+            src={imageProps.remoteImage}
             alt="Picture of the author"
-            blurDataURL={`data:image/jpeg:base64,${remoteImage}`}
+            blurDataURL={imageProps.blurDataURL}
             placeholder="blur"
             width={200}
             height={250}
           />
           <Image
-            src={remoteImage}
+            src={localImage}
             alt="Picture of the author"
-            blurDataURL={`data:image/jpeg:base64,${remoteImage}`}
             placeholder="blur"
             width={200}
             height={250}
@@ -78,12 +76,52 @@ const HomePage = () => {
       <section>
         <h2>4. Remote Images (Responsive layout type)</h2>
         <Image
-          src={remoteImage}
+          src={imageProps.remoteImage}
           alt="Picture of the author"
-          blurDataURL={`data:image/jpeg:base64,${remoteImage}`}
+          blurDataURL={imageProps.blurDataURL}
           placeholder="blur"
           layout="responsive"
           width={400}
+          height={500}
+        />
+      </section>
+
+      <section>
+        <h2>5. Local Images (Quality of the images [1-100])</h2>
+        <div>
+          <p>Image with the quality set to its lowest (1)</p>
+          <Image
+            src={localImage}
+            alt="Picture of the author"
+            width={800}
+            height={500}
+            quality={1}
+          />
+        </div>
+        <div>
+          <p>Image with the quality set to its maximum (100)</p>
+          <Image
+            src={localImage}
+            alt="Picture of the author"
+            width={800}
+            height={500}
+            quality={100}
+          />
+        </div>
+      </section>
+
+      <section>
+        <h2>6. Remote Images (onLoadComplete)</h2>
+        <Image
+          src={imageProps.remoteImage}
+          alt="Picture of the author"
+          blurDataURL={imageProps.blurDataURL}
+          placeholder="blur"
+          layout="responsive"
+          width={400}
+          onLoadingComplete={(image) => {
+            console.log(image);
+          }}
           height={500}
         />
       </section>
@@ -92,3 +130,19 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+export const getStaticProps = async () => {
+  const remoteImage =
+  "https://images.unsplash.com/photo-1525824236856-8c0a31dfe3be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHdhdGVyZmFsbHxlbnwwfHwwfHw%3D";
+  const { base64, img } = await getPlaiceholder(remoteImage, { size: 10 });
+
+  return {
+    props: {
+      imageProps: {
+        ...img,
+        blurDataURL: base64,
+        remoteImage
+      },
+    },
+  };
+};
